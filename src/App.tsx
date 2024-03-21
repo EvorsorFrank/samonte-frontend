d825c7ce765424d5d8e081d12507930026956cae
EvorsorFrank <programmer.franksamonte@gmail.com>
2024-03-21 19:28:18 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl font-bold'>
+          Create App with This!
+      </div>
+    </>
+  )
+}
+
+export default App

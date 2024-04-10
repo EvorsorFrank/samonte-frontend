12461a53a786c6385face2f5fc86c4993bc3ded4
EvorsorFrank <programmer.franksamonte@gmail.com>
2024-04-10 15:30:00 +0800

    Update endpointAPI.tsx

--- src/endpointAPI.tsx
+++ src/endpointAPI.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://88d5-2001-4452-3d4-5000-a5c1-a2e1-9def-859.ngrok-free.app';
+const BASE_URL = 'https://ee2e-49-150-254-56.ngrok-free.app';
 
 const endpointAPI = () => BASE_URL;
 
